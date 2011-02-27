fa368418f60c8e4a53389404019c530ecd19ad6c
john <john@onyx-international.com>
2011-02-27 22:14:11 +0800

    added screen update watcher class.

---
+++ code/include/onyx/screen/screen_update_watcher.h
@@ -0,0 +1,50 @@
+#ifndef ONYX_SCREEN_UPDATE_WATCHE_H_
+#define ONYX_SCREEN_UPDATE_WATCHE_H_
+
+#include <QWidget>
+#include "screen_proxy.h"
+
+namespace onyx
+{
+namespace screen
+{
+
+class ScreenUpdateWatcher
+{
+public:
+    static ScreenUpdateWatcher & instance()
+    {
+        static ScreenUpdateWatcher instance_;
+        return instance_;
+    }
+    ~ScreenUpdateWatcher();
+
+public:
+    void enqueue(QWidget *widget, onyx::screen::ScreenProxy::Waveform w);
+    void updateScreen();
+
+private:
+    ScreenUpdateWatcher();
+    ScreenUpdateWatcher(ScreenUpdateWatcher &ref);
+
+private:
+    struct UpdateItem
+    {
+        QWidget *widget;
+        onyx::screen::ScreenProxy::Waveform waveform;
+
+        UpdateItem(QWidget * wnd, onyx::screen::ScreenProxy::Waveform w)
+            : widget(wnd)
+            , waveform(w)
+        {}
+    };
+    QQueue<UpdateItem> queue_;
+};
+
+ScreenUpdateWatcher & watcher();
+
+};  // namespace screen
+
+};  // namespace onyx
+
+#endif
