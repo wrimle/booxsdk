43c599d4f702d4a086643ea80fec533abd208ca9
jim <qiujiejun@onyx-international.com>
2011-04-30 10:59:47 +0800

    fixed for typos.

--- code/include/onyx/data/keys.h
+++ code/include/onyx/data/keys.h
@@ -13,12 +13,12 @@ static const int KEY_ESC =        0x02;
 static const int KEY_PAGEUP =     0x04;
 static const int KEY_PAGEDOWN =   0x08;
 
-static const int ENABLE_ALL = (KEY_MENU | KEY_ESC | KEY_PAGEUP | KEY_PAGEUP);
+static const int ENABLE_ALL = (KEY_MENU | KEY_ESC | KEY_PAGEUP | KEY_PAGEDOWN);
 static const int ENABLE_MENU_ESC = (KEY_MENU | KEY_ESC);
-static const int ENABLE_PAGE_UP_DOWN = (KEY_PAGEUP | KEY_PAGEUP);
+static const int ENABLE_PAGE_UP_DOWN = (KEY_PAGEUP | KEY_PAGEDOWN);
 
 static const int DISABLE_MENU_ESC = (~KEY_MENU) & (~KEY_ESC);
-static const int DISABLE_PAGE_UP_DOWN = (~KEY_PAGEUP) & (~KEY_PAGEUP);
+static const int DISABLE_PAGE_UP_DOWN = (~KEY_PAGEUP) & (~KEY_PAGEDOWN);
 
 static const int DISABLE_ALL = 0;
 
