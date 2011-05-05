def36abe091e7b1a4b2e7eda5252c4887b7b834e
lion <lion@onyx-international.com>
2011-05-05 16:30:45 +0800

    fix M90 sound bug

--- code/src/sys/volume_conf.cpp
+++ code/src/sys/volume_conf.cpp
@@ -1,6 +1,7 @@
 
 #include "onyx/base/base.h"
 #include "onyx/sys/volume_conf.h"
+#include "onyx/sys/platform.h"
 
 
 namespace sys
@@ -56,17 +57,36 @@ QVector<int> VolumeConfig::volumes()
     static QVector<int> VOLUMES;
     if (VOLUMES.size() <= 0)
     {
-        VOLUMES.push_back(0);
-        VOLUMES.push_back(60);
-        VOLUMES.push_back(64);
-        VOLUMES.push_back(68);
-        VOLUMES.push_back(72);
-        VOLUMES.push_back(76);
-        VOLUMES.push_back(80);
-        VOLUMES.push_back(85);
-        VOLUMES.push_back(90);
-        VOLUMES.push_back(95);
-        VOLUMES.push_back(100);
+        if(sys::isIMX31L())
+        {
+            qDebug()<<"******************** M90/M91 platform *****************************"<<__TIME__;
+            VOLUMES.push_back(0);
+            VOLUMES.push_back(40);
+            VOLUMES.push_back(46);
+            VOLUMES.push_back(52);
+            VOLUMES.push_back(58);
+            VOLUMES.push_back(64);
+            VOLUMES.push_back(70);
+            VOLUMES.push_back(78);
+            VOLUMES.push_back(86);
+            VOLUMES.push_back(94);
+            VOLUMES.push_back(100);
+        }
+        else
+        {
+            qDebug()<<"******************** other platform *****************************"<<__TIME__;
+            VOLUMES.push_back(0);
+            VOLUMES.push_back(60);
+            VOLUMES.push_back(64);
+            VOLUMES.push_back(68);
+            VOLUMES.push_back(72);
+            VOLUMES.push_back(76);
+            VOLUMES.push_back(80);
+            VOLUMES.push_back(85);
+            VOLUMES.push_back(90);
+            VOLUMES.push_back(95);
+            VOLUMES.push_back(100);
+        }
     }
 
     return VOLUMES;
